a7e240328520941d1ddafc90a2f5724b80e7a151
Your Name <lisleclara1@gmail.com>
2023-09-06 17:32:14 -0300

    funcionalidad 6

--- pages/preguntas-frecuentes/index.page.tsx
+++ pages/preguntas-frecuentes/index.page.tsx
@@ -1,22 +1,53 @@
 import type {NextPage} from 'next'
-import Head from 'next/head'
-import BodySingle from "dh-marvel/components/layouts/body/single/body-single";
-import LayoutGeneral from 'dh-marvel/components/layouts/layout-general';
 import { FaqsType } from 'dh-marvel/components/faqs/faqsData';
-
+import { Accordion, AccordionSummary } from '@mui/material';
+import React from 'react';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Typography from '@mui/material/Typography';
+import AccordionDetails from '@mui/material/AccordionDetails';
 
 interface Props{
        faqs: FaqsType[]
     }
 
-const Index: NextPage<Props> = ({faqs}) => {    
-        console.log("hola"+faqs);
+const Index: NextPage<Props> = ({faqs}) => {  
+  const [expanded, setExpanded] = React.useState<number | false>(false);
+
+  const handleChange =
+    (panel: number) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+      setExpanded(isExpanded ? panel : false);
+    };  
+       
         
     return (
-        <>
-            <h1>hola</h1>    
+      <>
+          <div style={{ width: '80%' ,  marginTop: '20px'}} >
+          {faqs?.map((faq) =>(
+            <Accordion key={faq.id} sx={{ margin:'0 auto'}} expanded={expanded === faq.id} onChange={handleChange(faq.id)}>
+                <AccordionSummary
+                sx={{ margin:'0 auto'}}
+                  expandIcon={<ExpandMoreIcon/>}
+                  aria-controls="panel1bh-content"
+                  id="panel1bh-header"            >
+              
+              <Typography sx={{ color: 'text.secondary' , margin:'0 auto'}}>{faq.question}</Typography>
+            </AccordionSummary >
+            <AccordionDetails  sx={{ margin:'0 auto'}}>
+              <Typography >
+                {faq.answer}
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          
+          
+          ))}
+          </div>   
             
-        </>
+      </>
+            
+     
+    
+        
     )
 }
 
@@ -24,10 +55,9 @@ export default Index
 
 export const getStaticProps = async () => {  
 //   const response = await fetch(`
-const response = await fetch(
-    //  "http://localhost:3000/api/faqs"
+const response = await fetch( 
      "https://ctd-esp-fe3-final-claralisle.vercel.app/api/faqs"
-    //  "https://ctd-esp-fe3-final-swart.vercel.app/api/faqs"
+    
   )  
   
   const faqs = await response.json()
