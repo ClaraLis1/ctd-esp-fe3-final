290dba4036d86fadc4a7f57bc375850c66cf567e
Your Name <lisleclara1@gmail.com>
2023-09-06 10:03:03 -0300

    added api

--- pages/_document.page.tsx
+++ pages/_document.page.tsx
@@ -4,10 +4,13 @@ export default function Document() {
     return (
         <Html style={{height: '100%'}}>
             <Head>
-                <link
+                {/* <link
                     href="https://fonts.googleapis.com/css2?family=Roboto"
                     rel="stylesheet"
-                />
+                /> */}
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&family=Roboto&display=swap" rel="stylesheet"></link>
             </Head>
             <body style={{height: '100%'}}>
             <Main />
