2f3e14db96e899b3b46235c9b15a3a8a906467db
Your Name <lisleclara1@gmail.com>
2023-09-07 00:48:43 -0300

    func 1 SSR

--- components/ui/card/CardComic.tsx
+++ components/ui/card/CardComic.tsx
@@ -6,36 +6,44 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import { ImagesearchRoller } from "@mui/icons-material";
 
 interface Comic{
-    image: string
-    name: string
+    images: []
+    title: string
+    id: number
 }
 
 interface Props{
     comic:Comic
 }
-export const CardComic: FC = ({ name}:any) => {
+export const CardComic: FC<Props> = ({comic}) => {
+    
+    
     return (
-        <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        alt="green iguana"
-        height="140"
-        image="/static/images/cards/contemplative-reptile.jpg"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {name}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {name}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small" variant="contained">Comprar </Button>
-        <Button size="small" variant="contained" >Ver Detalle</Button>
-      </CardActions>
+    <Card sx={{ maxWidth: 345 ,  height:300 }}>
+        <CardMedia
+            component="img"
+             alt='imagen de Marvel'
+            height="140"
+                image="/static/images/cards/contemplative-reptile.jpg"
+            //  image={comic.images.length>0? comic.images[0].path:""}
+        />
+        <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+            {comic.title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+            {comic.title}
+            </Typography>
+        </CardContent>
+        <Grid  display="flex" justifyContent="flex-end" alignItems="center">
+        <CardActions >
+            <Button size="small" variant="contained">Comprar </Button>
+            <Button size="small" variant="contained" >Ver Detalle</Button>
+        </CardActions>
+        </Grid>
     </Card>
     )
 }
